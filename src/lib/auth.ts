9a0f7f611fe398ba805916e525b61868db745125
Alona Sudakova <alenasud725@gmail.com>
2025-04-22 23:52:27 +0200

    auth options export bugs fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 // lib/auth.ts
 import { type NextAuthOptions } from "next-auth"
-import { authOptions as nextAuthOptions } from '../app/api/auth/[...nextauth]/route'
+import { authOptions as nextAuthOptions } from '@/lib/authOptions'
 
 export const authOptions: NextAuthOptions = nextAuthOptions
